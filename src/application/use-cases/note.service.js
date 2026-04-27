210069d5062351783e113276212978d204aa0f3f
daniel german <danielgerman335@icloud.com>
2026-04-26 21:21:59 -0400

    se agrego metodo de update y delete en service de note

--- src/application/use-cases/note.service.js
+++ src/application/use-cases/note.service.js
@@ -17,4 +17,33 @@ export default class NoteService {
     async getNoteByUser(userId) {
         return await this.noteRepository.findByUserId(userId);
     }
+
+    async updateNote(id, data) {
+        if (!id) {
+            throw new Error('Note ID is required');
+        }
+
+        const existingNote = await this.noteRepository.findById(id);
+        if (!existingNote) {
+            throw new Error('Note not found');
+        }
+
+        existingNote.title = data.title ?? existingNote.title;
+        existingNote.content = data.content ?? existingNote.content;
+
+        return await this.noteRepository.update(id, existingNote);
+    }
+
+    async deleteNote(id) {
+        if (!id) {
+            throw new Error('Note ID is required');
+        }
+
+        const existingNote = await this.noteRepository.findById(id);
+        if (!existingNote) {
+            throw new Error('Note not found');
+        }
+
+        return await this.noteRepository.delete(id);
+    }
 }
